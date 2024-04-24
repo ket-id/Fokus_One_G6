b6479fb7b9b7ea2ffef9f416a86ff41214337d60
ket-id <ket-id@outlook.com>
2024-04-24 10:31:36 -0300

    Agrega funcion de botones 'active'

--- script.js
+++ script.js
@@ -124,22 +124,30 @@ const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonCorto = document.querySelector('.app__card-button--corto');
 const botonLargo = document.querySelector('.app__card-button--largo');
 const banner = document.querySelector('.app__image');
-const titulo = document.querySelector('.app__title')
+const titulo = document.querySelector('.app__title');
+const botones = document.querySelectorAll('.app__card-button');
 
 
 botonEnfoque.addEventListener('click', () => {
     cambiarContexto('enfoque');
+    botonEnfoque.classList.add('active');
 });
 
 botonCorto.addEventListener('click', () => {
     cambiarContexto('descanso-corto');
+    botonCorto.classList.add('active');
 });
 
 botonLargo.addEventListener('click', () => {
     cambiarContexto('descanso-largo');
+    botonLargo.classList.add('active');
 });
 
 function cambiarContexto(contexto) {
+    botones.forEach(function (contexto) {
+        contexto.classList.remove('active');
+    })
+
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `./imagenes/${contexto}.png`);
     switch (contexto) {
