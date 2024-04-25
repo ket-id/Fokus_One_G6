0e3849c75e9c843dcf88bb8a38c50cff5105cf0f
ket-id <ket-id@outlook.com>
2024-04-25 10:01:06 -0300

    Agrega el comienzo del temporizador y acomoda el codigo para que sea mas legible

--- script.js
+++ script.js
@@ -120,14 +120,21 @@
 //
 
 const html = document.querySelector('html');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonCorto = document.querySelector('.app__card-button--corto');
 const botonLargo = document.querySelector('.app__card-button--largo');
-const banner = document.querySelector('.app__image');
-const titulo = document.querySelector('.app__title');
 const botones = document.querySelectorAll('.app__card-button');
 const inputMusicaEnfoque = document.querySelector('#alternar-musica');
 const musica = new Audio('./sonidos/luna-rise-part-one.mp3');
+const botonIniciarPausar = document.querySelector('#start-pause');
+const audioPlay = new Audio('./sonidos/play.wav');
+const audioPausa = new Audio('./sonidos/pause.mp3');
+const audioTiempoFinalizado = new Audio('./sonidos/beep.mp3');
+
+let tiempoTranscurridoEnSegundos = 5;
+let idIntervalo = null;
 
 musica.loop = true;
 musica.volume = 0.5;
@@ -183,4 +190,31 @@ function cambiarContexto(contexto) {
         default:
             break;
     }
+}
+
+const cuentaRegresiva = () => {
+    if (tiempoTranscurridoEnSegundos <= 0) {
+        audioTiempoFinalizado.play();
+        reiniciar();
+        alert("¡Finalizo el tiempo!");
+        return;
+
+    }
+    tiempoTranscurridoEnSegundos -= 1;
+    console.log("temporizador: " + tiempoTranscurridoEnSegundos);
+}
+botonIniciarPausar.addEventListener("click", iniciarPausar);
+
+function iniciarPausar() {
+    if (idIntervalo) {
+        audioPausa.play();
+        reiniciar();
+        return;
+    }
+    audioPlay.play();
+    idIntervalo = setInterval(cuentaRegresiva, 1000);
+}
+function reiniciar() {
+    clearInterval(idIntervalo);
+    idIntervalo = null;
 }
